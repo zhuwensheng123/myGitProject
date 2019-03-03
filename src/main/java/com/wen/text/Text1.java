875ed9ca26efb85d0e3f36a12315dc01d2563c4c
ZhuWensheng <wsjava@163.com>
2019-03-03 15:18:35 +0800

    提交

--- src/main/java/com/wen/text/Text1.java
+++ src/main/java/com/wen/text/Text1.java
@@ -2,11 +2,8 @@ package com.wen.text;
 
 public class Text1 {
 	public static void main(String [] args) {
-		System.out.println("error");
-		System.out.println("dddd");
-		System.out.println("abcdefg");
-		System.out.println("error");
-		System.out.println("hhh");
+		System.out.println("dog dog dd nashishenmdao");
+		
 	}
    
 
